60e4f090684ae38b73c01d412e16a8413ddaeeed
zhaojianzz <1071438836@qq.com>
2022-05-13 14:20:48 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,60 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { visualizer } from 'rollup-plugin-visualizer'
+import Unocss from 'unocss/vite'
+import { chunkSplitPlugin } from "vite-plugin-chunk-split";
+import checker from "vite-plugin-checker";
+import viteCompression from "vite-plugin-compression";
+import legacy from "@vitejs/plugin-legacy";
+// https://vitejs.dev/config/
+const BUILD_VSL = process.env.BUILD_VSL !== undefined;
+const filter = /\.(js|css|json|txt|html|ico|svg|ttf|eot|woff)(\?.*)?$/i
+export default defineConfig({
+  plugins: [
+    vue(),
+    Components({
+      dts: resolve('./src/components/.conponents.d.ts'),
+      resolvers: [ ElementPlusResolver() ]
+    }),
+    BUILD_VSL && visualizer({
+      gzipSize: true,
+      brotliSize: true,
+      open: true,
+    }),
+    Unocss(),
+    chunkSplitPlugin(),
+    {
+      ...checker({
+        typescript: true,
+        eslint: {
+          lintCommand: 'eslint "./src/**/*.{vue,js,mjs,cjs,jsx,ts,tsx}"',
+          dev: {
+            logLevel: ["error"],
+          },
+        },
+      }),
+      apply: "serve",
+    },
+    viteCompression({
+      filter,
+      threshold: 10240
+    }),
+    legacy({
+      ignoreBrowserslistConfig: true,
+      targets: ["ie >= 11"],
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+    })
+  ],
+  resolve: {
+    alias:{
+      "@": resolve(__dirname, 'src'),
+      "@c": resolve(__dirname, 'src/components')
+    }
+  },
+  build: {
+    target: ['es2015']
+  }
+})
